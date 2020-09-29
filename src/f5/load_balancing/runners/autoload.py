1dcae31ba92a4a1188b45bb7b4c8d7fd4c9c66e0
Anthony Poddubny <anton.p@qualisystems.com>
2020-09-29 18:37:08 +0300

    rework shell with the cloudshell-f5 package

---
+++ src/f5/load_balancing/runners/autoload.py
@@ -0,0 +1,13 @@
+from cloudshell.f5.runners.f5_autoload_runner import AbstractF5AutoloadRunner
+
+from f5.load_balancing.flows.autoload import F5LoadBalancerSnmpAutoloadFlow
+
+
+class F5LoadBalancerAutoloadRunner(AbstractF5AutoloadRunner):
+    @property
+    def autoload_flow(self):
+        """
+
+        :return:
+        """
+        return F5LoadBalancerSnmpAutoloadFlow(snmp_handler=self.snmp_handler, logger=self._logger)
